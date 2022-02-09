030ea2025a506d0f28998499f02b03c458cb3361
bobby.li <bobby.lhan@gmail.com>
2022-02-09 23:15:27 +0800

    upd: EntryCard UI

--- src/EntryCard/EntryCard.vue
+++ src/EntryCard/EntryCard.vue
@@ -1,36 +1,83 @@
 <template>
-  <v-layout :class="classes()">
-    {{ members }}
+  <v-layout :class="classes()" row align-center>
+    <div :class="[classes('left', 'mr-4'), classes(type)].join(' ')" :style="`backgroundColor: ${themeColor};`" />
+    <v-layout column align-start>
+      <h3 :class="classes('title')">{{ title }}</h3>
+      <div :class="classes('member', 'mt-3')">
+        <v-avatar
+          v-for="(url, index) in avatars"
+          :key="index"
+          :class="classes('member-avatar', index === 0 ? '' : 'ml-n4')"
+          size="36"
+        >
+          <v-img v-if="url" :src="url" />
+          <i v-else :class="classes('member-avatar-placeholder')" />
+        </v-avatar>
+        <span :class="classes('member-total', 'ml-3')">{{ memberTxt }}</span>
+      </div>
+    </v-layout>
   </v-layout>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
-  onMounted
+  onMounted,
+  PropType
 } from '@vue/composition-api';
 import classnames from '@utils/classnames';
-import { VLayout } from 'vuetify/lib';
+import { VAvatar, VLayout, VImg } from 'vuetify/lib';
 import { $t } from '@locale/index';
 
 export default defineComponent({
   name: 'EntryCard',
   components: {
+    VAvatar,
     VLayout,
+    VImg,
   },
   props: {
+    title: {
+      type: String,
+      default: '#XXX',
+    },
+    members: {
+      type: Object as PropType<{
+        avatars: string[];
+        total: number;
+      }>,
+      default: {
+        avatars: ['', '', ''],
+        total: 0,
+      } as {
+        avatars: string[];
+        total: number;
+      },
+    },
+    themeColor: {
+      type: String,
+      default: '#F5F5F5',
+    },
+    type: {
+      type: String,
+      default: 'chat',
+    },
     prefixCls: {
       type: String,
       default: 'entrycard'
     }
   },
   setup(props) {
-    const classes = classnames(props.prefixCls);
+    const { 
+      members,
+      prefixCls
+    } = props;
+    const classes = classnames(prefixCls);
     onMounted(() => {
       console.info('EntryCard mounted!');
     });
 
-    return { classes, members: `156 ${$t('members')}` };
+    return { classes, memberTxt: `${members.total} ${$t('members')}`, avatars: members.avatars };
   }
 });
 </script>
