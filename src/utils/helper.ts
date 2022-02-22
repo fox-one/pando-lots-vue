dbc7c06cb3555e61b55237fde2fe35be57b0626b
bobby.li <bobby.lhan@gmail.com>
2022-02-22 17:54:15 +0800

    [@FOXONE/PANDO-LOTS-VUE]: 0.0.1-alpha.21

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,7 +3,7 @@ export function getQuerystring(qs: string) {
   const querystring = location.href
     .split(new RegExp(`((&|\\?){1}${qs}=)`))?.[3]
     ?.split('&')?.[0]
-    ?.split('/#')?.[0]
+    ?.split('#')?.[0]
     ?.replace(/\/$/g, '')
     ?.replace(/\?/g, '%3F');
   return querystring || '';
